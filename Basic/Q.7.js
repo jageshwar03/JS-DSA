45fe84a74f4a58c391fa18978ca814e974dca5b4
jageshwar03 <jageshwarkumarsaw62@gmail.com>
2025-08-14 13:04:27 +0530

    Q.7 update

---
+++ Basic/Q.7.js
@@ -0,0 +1,8 @@
+
+
+
+// Accept user's name and age, and print: Hello Kurukshetra, you are 2 years old.
+
+let user=prompt("Enter user Name");
+
+console.log("Hello Kurukshtra you are two year old",user);
